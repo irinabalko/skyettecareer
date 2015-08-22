db2ae6ca8036f682daa1eac934505afe139b8520
Irina Balko <irinabalko@Irinas-MBP.fritz.box>
2015-08-22 16:27:38 +0200

    Ready to push!

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+import core.views as coreviews
 
 urlpatterns = patterns('',
 
-	url(r'^admin/', include(admin.site.urls)),
-	(r'', include('core.urls')),
+ url(r'^$', coreviews.LandingView.as_view()),
 )
