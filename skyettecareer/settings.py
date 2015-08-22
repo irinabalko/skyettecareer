e7140af73de4ddeb3467b013f73e451d10683bbc
Irina Balko <irinabalko@Irinas-MBP.fritz.box>
2015-08-22 22:03:53 +0200

    Trying to fix admin error

--- skyettecareer/settings.py
+++ skyettecareer/settings.py
@@ -80,12 +80,12 @@ if ON_HEROKU == '1':
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
 else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
